b40f6a0787380aac59f9c006a8bce70450250a22
Minhaj Khalifa <34818878+minhajkhalifa@users.noreply.github.com>
2018-03-12 17:03:27 +0000

    RaceTrack

---
+++ Game Dev Coursework/Assets/_Scripts/CarCreationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class CarCreationController : MonoBehaviour {
+
+    public GameObject[] cars;
+    public int carCounter;
+    public int selectedCar; //Set selected car
+
+	// Use this for initialization
+	void Start () {
+        carCounter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            cars[carCounter].SetActive(false);
+            carCounter++;
+            if (carCounter >= cars.Length)
+            {
+                carCounter = 0;
+            }
+            cars[carCounter].SetActive(true);
+
+            //PlayerPrefs.SetInt("CarSelection", carCounter);
+
+        }
+	}
+}
--- Game Dev Coursework/Assets/_Scripts/Globals.cs
+++ Game Dev Coursework/Assets/_Scripts/Globals.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Globals : MonoBehaviour {
     public const string MENU_SCENE = "Menu";
     public const string CAR_SELECTION_SCENE = "CarSelection";
-    public const string GAME_SCENE = "Game";
+    public const string GAME_SCENE = "RaceTrack";
     public const string HELP_SCENE = "Help";
     public const string PLAYER_TYPE_SCENE = "PlayerTypeMenu";
 }
