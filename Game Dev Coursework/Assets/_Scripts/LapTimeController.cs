3eaef0820af66ec24b13fa10bdb8dd19734d7e58
Minhaj Khalifa <34818878+minhajkhalifa@users.noreply.github.com>
2018-03-14 01:11:12 +0000

    Lap Timer stuff

---
+++ Game Dev Coursework/Assets/_Scripts/LapTimeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapTimeController : MonoBehaviour {
+
+    public Text lapTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int minutes = Mathf.FloorToInt(Time.timeSinceLevelLoad / 60);
+        float seconds = Mathf.FloorToInt(Time.timeSinceLevelLoad % 60);
+        float milliSeconds = Mathf.FloorToInt(Time.timeSinceLevelLoad * 10);
+
+        lapTime.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliSeconds);
+    }
+}
