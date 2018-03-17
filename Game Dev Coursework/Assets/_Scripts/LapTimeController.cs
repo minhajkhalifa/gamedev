bce2904797246888568e0457f06d9fdf5b453a9c
Minhaj Khalifa <34818878+minhajkhalifa@users.noreply.github.com>
2018-03-17 18:17:37 +0000

    Got the young countdown working

--- Game Dev Coursework/Assets/_Scripts/LapTimeController.cs
+++ Game Dev Coursework/Assets/_Scripts/LapTimeController.cs
@@ -1,23 +1,66 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityStandardAssets.Vehicles.Car;
 
 public class LapTimeController : MonoBehaviour {
 
+    public GameObject carControl;
+    public GameObject carControl2;
+    public GameObject lapTiming;
     public Text lapTime;
+    bool isGamePlaying;
+    public Text startTimer;
+    public int countDown = 3;
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        //carControl.SetActive(false);
+        CarController carController = carControl.GetComponent<CarController>();
+        CarController car2Controller = carControl2.GetComponent<CarController>();
+        carController.enabled = false;
+        car2Controller.enabled = false;
+        //carControl2.SetActive(false);
+        lapTiming.SetActive(false);
+        startTimer.text = countDown.ToString();
+        StartCoroutine(Countdown(countDown));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        int minutes = Mathf.FloorToInt(Time.timeSinceLevelLoad / 60);
-        float seconds = Mathf.FloorToInt(Time.timeSinceLevelLoad % 60);
-        float milliSeconds = Mathf.FloorToInt(Time.timeSinceLevelLoad * 10);
 
-        lapTime.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliSeconds);
+    private IEnumerator Countdown(int seconds)
+    {
+        for (int i = seconds; i >= 0; i--)
+        {
+            if (i > 0)
+            {
+                startTimer.text = i.ToString();
+                yield return new WaitForSeconds(1.0f);
+            }
+            else if (i <= 0)
+            {
+                startTimer.text = "GO";
+                yield return new WaitForSeconds(1.0f);
+                isGamePlaying = true;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (isGamePlaying)
+        {
+            startTimer.text = "";
+            CarController carController = carControl.GetComponent<CarController>();
+            CarController car2Controller = carControl2.GetComponent<CarController>();
+            carController.enabled = true;
+            car2Controller.enabled = true;
+            lapTiming.SetActive(true);
+            int minutes = Mathf.FloorToInt(Time.timeSinceLevelLoad / 60);
+            float seconds = Mathf.FloorToInt(Time.timeSinceLevelLoad % 60);
+            float milliSeconds = Mathf.FloorToInt(Time.timeSinceLevelLoad * 10);
+
+            lapTime.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliSeconds);
+        }
     }
 }
