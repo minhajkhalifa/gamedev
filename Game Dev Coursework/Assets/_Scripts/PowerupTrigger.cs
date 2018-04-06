45925af4d46f9fb3dd0eb5e5f2d44b7a1af0064c
Minhaj Khalifa <34818878+minhajkhalifa@users.noreply.github.com>
2018-04-07 00:45:01 +0100

    Got Powerups BOY

--- Game Dev Coursework/Assets/_Scripts/PowerupTrigger.cs
+++ Game Dev Coursework/Assets/_Scripts/PowerupTrigger.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class PowerupTrigger : MonoBehaviour {
 
+    public GameObject powerup;
+
     private void OnTriggerEnter(Collider other)
     {
         var carRB = other.transform.parent.parent.GetComponent<Rigidbody>();
         var vehicle = other.transform.parent.parent;
         vehicle.GetComponent<Rigidbody>().AddRelativeForce(transform.forward * 500, ForceMode.Acceleration);
+
+        powerup.GetComponent<Renderer>().enabled = false;
+        powerup.GetComponentInChildren<ParticleSystem>().Stop();
+        StartCoroutine(HideShow());
+    }
+
+    IEnumerator HideShow()
+    {
+        yield return (new WaitForSeconds(3));
+        powerup.GetComponent<Renderer>().enabled = true;
+        powerup.GetComponentInChildren<ParticleSystem>().Play();
     }
 
     // Use this for initialization
