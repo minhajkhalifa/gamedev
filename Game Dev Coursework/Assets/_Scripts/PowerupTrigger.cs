5efc6e21f10e704b933286e16be54299f10977bd
Minhaj Khalifa <34818878+minhajkhalifa@users.noreply.github.com>
2018-04-06 19:30:22 +0100

    Speed Power Racing

---
+++ Game Dev Coursework/Assets/_Scripts/PowerupTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var carRB = other.transform.parent.parent.GetComponent<Rigidbody>();
+        var vehicle = other.transform.parent.parent;
+        vehicle.GetComponent<Rigidbody>().AddRelativeForce(transform.forward * 500, ForceMode.Acceleration);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
